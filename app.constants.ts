d5f72f040216b2ff03409f51b2bc4c1edfb63437
Kirill <pesterevkv2003@gmail.com>
2024-07-19 13:34:02 +0300

    upd: add backend

---
+++ app.constants.ts
@@ -0,0 +1,6 @@
+export const APPWRITE_ID = "669a399700046fd8eacc";
+export const DB_ID = "669a3cc00024ca9abfae";
+export const COLLECTION_DEALS = "deals";
+export const COLLECTION_CUSTOMERS = "customers";
+export const COLLECTION_COMMENTS = "comments";
+export const STORAGE_ID = "storage";
