776553b79ab3b1ffea2a07b9f08faf1783ad13ba
Kirill <pesterevkv2003@gmail.com>
2024-07-20 13:58:43 +0300

    upd: added cards

---
+++ components/ui/card/Card.vue
@@ -0,0 +1,41 @@
+<script setup lang="ts">
+import type { HTMLAttributes } from "vue";
+import { cn } from "@/utils";
+
+const props = defineProps<{
+  class?: HTMLAttributes["class"];
+}>();
+</script>
+
+<template>
+  <div
+    :class="
+      cn(
+        'rounded-lg border border-transparent border-gray-400 bg-white text-gray-950 shadow dark:border-gray-800 dark:bg-gray-950 dark:text-gray-50 transition-colors hover:bg-gray-50 dark:hover:bg-gray-800',
+        props.class
+      )
+    "
+  >
+    <slot />
+  </div>
+</template>
+
+<style scoped>
+@keyframes show {
+  from {
+    @apply border-gray-400;
+    opacity: 0.4;
+    transform: scale(0.8) translateY(-30px);
+  }
+
+  90% {
+    @apply border-gray-400;
+  }
+
+  to {
+    @apply border-transparent;
+    transform: scale(1) translateY(0);
+    opacity: 1;
+  }
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,17 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div class="p-10"><h1 class="text-3xl font-bold">My Nuxt project</h1></div>
+  <div class="p-10">
+    <h1 class="text-3xl font-bold">My Nuxt project</h1>
+    <div>
+      <UiCard class="mb-3 bg-gray-300">
+        <UiCardHeader> Card title </UiCardHeader>
+        <UiCardContent>
+          <p class="text-gray-700 dark:text-gray-400">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus
+          </p>
+        </UiCardContent>
+      </UiCard>
+    </div>
+  </div>
 </template>
