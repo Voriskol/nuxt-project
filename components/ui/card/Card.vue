e07aa8cbea86be0537c13946860936665fdfa4cb
Kirill <pesterevkv2003@gmail.com>
2024-07-20 14:22:43 +0300

    upd: added interfaces and styles

--- components/ui/card/Card.vue
+++ components/ui/card/Card.vue
@@ -11,7 +11,7 @@ const props = defineProps<{
   <div
     :class="
       cn(
-        'rounded-lg border border-transparent border-gray-400 bg-white text-gray-950 shadow dark:border-gray-800 dark:bg-gray-950 dark:text-gray-50 transition-colors hover:bg-gray-50 dark:hover:bg-gray-800',
+        'rounded-lg border border-transparent bg-gray-400 border-gray-400 bg-white text-gray-950 shadow dark:border-gray-800 dark:bg-gray-950 dark:text-gray-50 transition-colors hover:bg-gray-50 dark:hover:bg-gray-800',
         props.class
       )
     "
--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,19 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import type { ICard, IColumn } from "~/components/kanban/kanban.types.ts";
+useHead({
+  title: "Home",
+});
+
+const dragCard = ref<ICard | null>(null);
+const sourceColumn = ref<IColumn | null>(null);
+</script>
 
 <template>
   <div class="p-10">
     <h1 class="text-3xl font-bold">My Nuxt project</h1>
     <div>
-      <UiCard class="mb-3 bg-gray-300">
-        <UiCardHeader> Card title </UiCardHeader>
+      <UiCard class="mb-3 bg-gray-300 hover:border-purple-400" draggable="true">
+        <UiCardHeader role="button"> Card title </UiCardHeader>
         <UiCardContent>
           <p class="text-gray-700 dark:text-gray-400">
             Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus
